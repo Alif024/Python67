d46eddcf9066ed32c8645d9d5ce0330120b507d0
Alif024 <aleefrock12345@gmail.com>
2024-08-15 21:24:58 +0700

    fix HW2

--- HomeWork/HW2/HW2-6706022510034.py
+++ HomeWork/HW2/HW2-6706022510034.py
@@ -1,4 +1,5 @@
-def length_of_longest_substring_k_distinct(s: str, k: int):
+def length_of_longest_substring_k_distinct(s: str, k: int) -> tuple:
+    # Your implementation here
     if k == 0:
         return (0, [])
     list_all_answer = []
@@ -10,27 +11,23 @@ def length_of_longest_substring_k_distinct(s: str, k: int):
             set_answer = set(list(scenario_answer))
             if len(set_answer) <= k:
                 list_answer.append(scenario_answer)
-            # print(set_answer)
-        # print(list_answer)
         list_all_answer.append(list_answer[-1])
-    # print(list_all_answer)
     highest_len = 0
     for answer in list_all_answer:
         if len(answer) >= highest_len:
             highest_len = len(answer)
-    # print(highest_len)
     result = []
     for answer in list_all_answer:
         if len(answer) == highest_len:
             result.append(answer)
-    # print(result)
     return (highest_len, result)
+
 
 s = "eceba"
 k = 2
 print(length_of_longest_substring_k_distinct(s, k))
 
-s = "aa"
+s = "aabbcc"
 k = 1
 print(length_of_longest_substring_k_distinct(s, k))
 

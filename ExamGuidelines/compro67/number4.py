c8cc38a057d742cff4ccbad357e266bdadff09bc
Alif024 <aleefrock12345@gmail.com>
2024-08-28 21:27:26 +0700

    #

--- ExamGuidelines/compro67/number4.py
+++ ExamGuidelines/compro67/number4.py
@@ -1,68 +1,3 @@
-def analyze_purchases(purchases: list) -> dict:
-    dict_purchases = {}
-    for cust_id, category, product in purchases:
-        if cust_id not in dict_purchases:
-            dict_purchases[cust_id] = dict([])
-        if category not in dict_purchases[cust_id]:
-            dict_purchases[cust_id][category] = []
-        dict_purchases[cust_id][category].append(product)
-    # print(dict_purchases)
-
-    dict_analyze = {}
-    all_buy_product = []
-    for cust_id, values in dict_purchases.items():
-        # print(cust_id, values)
-        # print(list(values.keys())[0])     # get category
-        # print(len(list(values.keys())))   # get len of category
-        high_frequen = 0
-        set_products_frequen = set([])
-        for category in list(values.keys()):
-            # print(category)     # get category
-            # print(dict_purchases[ust_id][category])     # get list products
-            for product in dict_purchases[cust_id][category]:
-                # print(cust_id, category, product)
-                all_buy_product.append((category, product))
-                if dict_purchases[cust_id][category].count(product) > high_frequen:
-                    high_frequen = dict_purchases[cust_id][category].count(
-                        product)
-                    set_products_frequen = set(
-                        [(category, product, high_frequen)])
-                elif dict_purchases[cust_id][category].count(product) == high_frequen:
-                    set_products_frequen.add((category, product, high_frequen))
-        if len(set_products_frequen) == 1:
-            dict_analyze[cust_id] = {list(set_products_frequen)[0][0]: list(set_products_frequen)[0][2]}
-        else:
-            dict_analyze[cust_id] = {}
-        # print(category_product_high_frequency, high_frequen)
-        # print(set_products_frequen)
-        # print(len(set_products_frequen))
-
-    dict_all_buy_product = {}
-    for category, product in all_buy_product:
-        if category not in dict_all_buy_product:
-            dict_all_buy_product[category] = {}
-        if product not in dict_all_buy_product[category]:
-            dict_all_buy_product[category][product] = 1
-        else:
-            dict_all_buy_product[category][product] += 1
-    # print(all_buy_product)
-    # print(dict_all_buy_product)
-
-    dict_most_frequency = {}
-    for category, values in dict_all_buy_product.items():
-        # print(category, values)
-        frequency_rate = max([rate for rate in values.values()])
-        # print(frequency_rate)
-        for product, frequency in values.items():
-            if frequency == frequency_rate:
-                dict_most_frequency.update({category: product})
-    # print(list_most_frequency)
-
-    dict_analyze["most_frequent"] = dict_most_frequency
-    # print(dict_analyze)
-    return dict_analyze
-
-
 purchases = [
     ("cust1", "electronics", "laptop"),
     ("cust2", "groceries", "apple"),
@@ -74,6 +9,57 @@ purchases = [
     ("cust3", "groceries", "apple"),
     ("cust3", "electronics", "camera"),
 ]
+
+def analyze_purchases(purchases: list) -> dict:
+    dict_purchases = {}
+    all_buy_product = []
+    for cust, category, item in purchases:
+        if cust not in dict_purchases:
+            dict_purchases[cust] = {}
+        if category not in dict_purchases[cust]:
+            dict_purchases[cust][category] = {}
+        if item not in dict_purchases[cust][category]:
+            dict_purchases[cust][category][item] = 1
+        else:
+            dict_purchases[cust][category][item] += 1
+        all_buy_product.append((category, item))
+    top_custs_category = {}
+    for cust in dict_purchases:
+        top_cat_cust = {}
+        for category in dict_purchases[cust]:
+            cat_count_list = []
+            for item in dict_purchases[cust][category]:
+                cat_count_list.append(dict_purchases[cust][category][item])
+            max_cat = max(cat_count_list)
+            list_top_cat_cust = []
+            for item in dict_purchases[cust][category]:
+                if dict_purchases[cust][category][item] == max_cat:
+                    list_top_cat_cust.append(item)
+            if len(list_top_cat_cust) == 1:
+                top_cat_cust[category] = max_cat
+            else:
+                top_cat_cust[category] = {}
+        if len(top_cat_cust) == 1:
+            top_custs_category[cust] = top_cat_cust
+        else:
+            top_custs_category[cust] = {}
+    dict_all_buy_product = {}
+    for category, product in all_buy_product:
+        if category not in dict_all_buy_product:
+            dict_all_buy_product[category] = {}
+        if product not in dict_all_buy_product[category]:
+            dict_all_buy_product[category][product] = 1
+        else:
+            dict_all_buy_product[category][product] += 1
+    dict_most_frequency = {}
+    for category, values in dict_all_buy_product.items():
+        frequency_rate = max([rate for rate in values.values()])
+        for product, frequency in values.items():
+            if frequency == frequency_rate:
+                dict_most_frequency.update({category: product})
+    top_custs_category["most_frequent"] = dict_most_frequency
+    return top_custs_category
+
 
 if __name__ == "__main__":
     print(analyze_purchases(purchases))

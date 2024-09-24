d61c9dff8e7a5214b3533c00eb922ed544980150
Alif024 <aleefrock12345@gmail.com>
2024-09-24 11:41:52 +0700

    #

--- HomeWork/HW5/maze.py
+++ HomeWork/HW5/maze.py
@@ -1,8 +1,10 @@
 from collections import deque
 
 def maze_solver_with_teleport(maze: list, portals: dict):
+    from random import choice
     path = []
     possible_moves = []
+    fork_points = {}
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     for index_row, row in enumerate(maze):
         for index_col, col in enumerate(row):
@@ -16,19 +18,29 @@ def maze_solver_with_teleport(maze: list, portals: dict):
             if col == '.':
                 possible_moves.append((index_row, index_col))
     count = 0
-    while count < 3:
+    while count < 5:
         posible_directions = []
         for direction in directions:
-            if (start[0] + direction[0], start[1] + direction[1]) in possible_moves:
+            # print(f"direction: {start[0] + direction[0], start[1] + direction[1]}")
+            if (start[0] + direction[0], start[1] + direction[1]) in possible_moves and (start[0] + direction[0], start[1] + direction[1]) not in path:  # if the next step is a valid move
                 posible_directions.append((start[0] + direction[0], start[1] + direction[1]))
         print(f"the current position: {start}")        # print the current position
         print(f"possible directions: {posible_directions}")         
         if len(posible_directions) == 1:    # if there is only one possible direction
             start = posible_directions[0]   # step to possible direction
             path.append(start)              # add the step to the path
-
+        elif len(posible_directions) == 0:  # if there is no possible direction
+            start = portals[start]          # step to the portal
+            path.append(start)              # add the step to the path
+        elif len(posible_directions) > 1:
+            rand_direction = choice(posible_directions)
+            fork_points[start] = rand_direction
+            start = rand_direction
+            path.append(start)
         print(f"the new position: {start}")        # print the current position                       
-        print(f"Path: {path}")                        # print the path
+        print(f"Path: {path}")                     # print the path
+        print(f"Fork Points: {fork_points}") 
+        print()
         count += 1
     # print(possible_moves)
     pass
@@ -56,8 +68,8 @@ if __name__ == "__main__":
     # # Example 2
     # maze = [
     #     ['S', '.', '#', 'P', '#', 'P'],
-    #     ['#', '.', '#', '.', '#', '.'],
-    #     ['#', '.', 'P', '.', '.', 'E'],
+    #     ['#', '.', '#', '.', '#', '#'],
+    #     ['#', '.', 'P', '.', '#', 'E'],
     #     ['P', '#', '#', '#', '#', '#'],
     #     ['#', '.', '.', 'P', '.', '.']
     # ]
